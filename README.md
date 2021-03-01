fe5d389220edd1360141306dd3f4d0ab5fdd63b6
Celeste Brant <celeste@celestes-mbp.lan>
2021-03-01 00:15:19 +0000

    complete

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Simon Game
-UNDER DEVELOPMENT!
+How to play
+1. Navigate to [the Simon Game](https://celestebrant.github.io/simon-game/)
+1. Press any key to start the game.
+1. Simon will give the first signal. Repeat the signal by pressing the same coloured button.
+1. Simon will give a new signal. Repeat the first followed by the new signal.
+1. The sequence of signals that Simon will build on will grow and your goal is to try to reproduce the signal sequence perfectly.
+1. Press any wrong button, and it's GAME OVER!
+
 This was created for jQuery practice.
--- game.js
+++ game.js
@@ -2,34 +2,56 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userPattern = [];
 var level = 0;
-var randomChosenColour;
-var match;
+var started = false;
 
+// Main bit
 $(document).keypress(function (event) {
-    randomChosenColour = nextSequence();
-
+    if (!started) {
+        nextSequence();
+        started = true;
+    }
 });
-
 
 $(".btn").click(function () {
     var userChosenColour = $(this).attr("id");
     userPattern.push(userChosenColour);
     animateClick(userChosenColour);
-
-    if (userChosenColour === randomChosenColour) {
-        playSound(userChosenColour);
-        console.log("HOORAY!");
-    } else {
-        playSound("wrong");
-        console.log("BOO!");
-    }
-
-    console.log(userPattern);
+    checkAnswer(userPattern.length - 1);
 });
 
+// Functions that will be called
+function startOver() {
+    level = [];
+    gamePattern = [];
+    started = false;
+}
+
+function checkAnswer(currentLevel) {
+    if (userPattern[currentLevel] === gamePattern[currentLevel]) {
+        console.log("success");
+        playSound(userPattern[currentLevel]);
+
+        if (userPattern.length === gamePattern.length) {
+            setTimeout(function () {
+                nextSequence();
+            }, 1500);
+        }
+    } else {
+        console.log("wrong");
+        playSound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(function () {
+            $("body").removeClass("game-over");
+        }, 500);
+        $("#level-title").text("Game over, press any key to restart");
+        startOver();
+    }
+}
+
 function nextSequence() {
+    userPattern = [];
     level++;
-    $("h1").text("Level " + level);
+    $("#level-title").text("Level " + level);
 
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber];
@@ -38,7 +60,6 @@ function nextSequence() {
     animateClick(randomChosenColour);
     playSound(randomChosenColour);
 
-    console.log(randomChosenColour, level);
     return randomChosenColour;
 }
 
